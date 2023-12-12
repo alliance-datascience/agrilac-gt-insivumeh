c702661ba9158b3c0b2967c7b6da6229542fe929
Sofia-Tzorin <110509743+Sofia-Tzorin@users.noreply.github.com>
2023-12-12 17:32:53 -0600

    Update 1_extraccion_datos_kobo.py

--- scripts/python/extract/1_extraccion_datos_kobo.py
+++ scripts/python/extract/1_extraccion_datos_kobo.py
@@ -10,12 +10,12 @@ import routine as rou
 import control_hora as control
 import lluvias_parciales as parcial
 
-token='e6f030307158e83c473c76789f11159a4c945e50'
+token=
 
-ruta_contenedora='/home/joshc/automatizacion_kobo/'
+ruta_contenedora='/home/'
 
 #Ruta de salida para todos los archivos creados durante los procesos.
-ruta_salida="/home/joshc/prueba_salida/"
+ruta_salida="/home/"
 
 #Nombre de los archivos crudos que se descargan de KOBO
 name_sinop= ruta_salida +'Formulario_estaciones_sinopticas.csv'
@@ -38,8 +38,8 @@ def descarga_datos(ruta_salida,ruta):
 
 
     #Se le dan las credenciales y el token del propitario del formulario de KOBO
-    #ruta='/home/joshc/automatizacion_kobo/'
-    os.system('curl -X GET https://kobo.humanitarianresponse.info/api/v2/assets.json -H "Authorization: Token e6f030307158e83c473c76789f11159a4c945e50" >'+ ruta+'apiDic.json')
+    #ruta=
+    os.system('curl -X GET
     #Se almacena todo el contenido de la API de KOBO dentro del archivo "apiDic.json"
     dicjson = open(ruta+'apiDic.json')
     apiDic = json.load(dicjson)
@@ -56,8 +56,8 @@ def descarga_datos(ruta_salida,ruta):
         link = apiDic['results'][i]['export_settings'][0]['data_url_csv']
        
         #Se les da permisos para podes descargar los formularios a traves de la ruta obtenida anteriormente
-        #os.system('wget -P '+ruta+ ' -d --header="Authorization: Token a2d815357d35aa5773ba65f1c5537b267eb1c9eb" '+link)
-        os.system('wget -P '+ruta+ ' -d --header="Authorization: Token '+token+'" '+link)
+        #os.system('wget -P '+ruta+ ' -d --header="Authorization: " '+link)
+        os.system('wget -P '+ruta+ ' -d --header="Authorization: Token '" '+link)
         #Se renombran los archivos que se deescargan a los nombres de su respectivo formulario
         os.rename(ruta+'data.csv',ruta_salida+nom_formulario+'.csv' )
         #print('\n \n'+'\n'+link+'\n \n \n')
